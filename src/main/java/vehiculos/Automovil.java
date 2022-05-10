f5b5fb65fa24f92d0f3e23ed47039f9274225f5a
Andres Felipe Aparicio Mestre <anapariciom@unal.edu.co>
2022-05-10 18:05:50 -0500

    commit 2

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -4,8 +4,8 @@ public class Automovil extends Vehiculo {
 	private int puestos;
 	private static int cantidad;
 
-	public Automovil(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso,
-			String traccion, Fabricante fabricante, int puestos) {
+	public Automovil(String placa, String nombre, int precio, int peso,
+			Fabricante fabricante, int puestos) {
 		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
 		this.puestos = puestos;
 		
